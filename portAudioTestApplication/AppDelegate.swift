9b2da21c130152bbd846fff1610ffa9665e76033
Julien Bloit <julien.bloit@gmail.com>
2019-10-01 09:49:51 +0200

    Initial Commit

---
+++ portAudioTestApplication/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  portAudioTestApplication
+//
+//  Created by julien@macmini on 01/10/2019.
+//  Copyright © 2019 jbloit. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ portAudioTestApplication/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  portAudioTestApplication
+//
+//  Created by julien@macmini on 01/10/2019.
+//  Copyright © 2019 jbloit. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
